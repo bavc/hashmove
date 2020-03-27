da2da1046d82c060eafa7d98dfae9e52da276758
iamdamosuzuki <ultraworld@Ultras-iMac.local>
2020-03-27 12:01:30 -0700

    changed hashcopy to hashsync. it's cuter

--- hashsync.py
+++ hashsync.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#hashcopy.py
+#hashsync.py
 #better file movement
 #takes arguments for source or dest, source can be file or dir, dest must be dir
 #copies files, hashes pre and post copy, deletes if hashes match, deletes dir if empty
@@ -29,7 +29,7 @@ import subprocess
 #generate list of destination files of pairs of start and end files
 def makeFileList(sourceList,dest,hashalg,hashlengths,excludeRoot):
     '''
-returns a list of file(s) to hashcopy
+returns a list of file(s) to hashsync
     '''
     flist = []
     for s in sourceList:
